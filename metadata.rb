c5ded68c93384e3939abd2074bab4fb5534a407a
Chad Gibbons <dcgibbons@gmail.com>
2013-04-29 14:58:37 -0600

    allow the copperegg template to be updated if attributes changes

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "support@copperegg.com"
 license          "MIT"
 description      "Installs/Configures CopperEgg services"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.0"
+version          "0.2.1"
 
 recipe "copperegg::default", "Installs CopperEgg collector binary"
 
