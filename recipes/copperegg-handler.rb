7e11a23e79feeb1eb54056b90f7c26c792e060c4
scott johnson <sjohnson@copperegg.com>
2013-07-11 09:34:25 -0500

    Fixes the problem of annotations not working on some chef installs.

--- recipes/copperegg-handler.rb
+++ recipes/copperegg-handler.rb
@@ -7,25 +7,33 @@
 #
 include_recipe 'chef_handler'
 
-r = gem_package 'chef-handler-copperegg' do
-  action :nothing
+
+# When running on Chef versions below 10.10, we have to use the gem_package workaround to install our handler,
+# as chef_gem was not introduced until 10.10.
+# See http://www.opscode.com/blog/2009/06/01/cool-chef-tricks-install-and-use-rubygems-in-a-chef-run/
+#
+
+if(Gem::Version.new(Chef::VERSION) < Gem::Version.new('0.10.9'))
+  r = gem_package 'chef-handler-copperegg' do
+    action :nothing
+  end
+  r.run_action(:install)
+  Gem.clear_paths
+else
+  chef_gem 'chef-handler-copperegg'
 end
-r.run_action(:install)
-
-Gem.clear_paths
-
 require 'chef/handler/copperegg'
 
 hostname = node.name
 if node.attribute?('ec2') && node.ec2.attribute?('instance_id')
   hostname = hostname + ' (' + node['ec2']['instance_id'] + ')'
 end
-      
+
 
 chef_handler 'Chef::Handler::Copperegg' do
-  arguments ['apikey' => node['copperegg']['apikey'], 
-            'annotate_success' => node['copperegg']['annotate_chefrun_success'], 
-            'annotate_fail' => node['copperegg']['annotate_chefrun_fail'], 
+  arguments ['apikey' => node['copperegg']['apikey'],
+            'annotate_success' => node['copperegg']['annotate_chefrun_success'],
+            'annotate_fail' => node['copperegg']['annotate_chefrun_fail'],
             'tags' =>  node['copperegg']['alltags'],
             'hostname' => hostname  ]
   source 'chef/handler/copperegg'
